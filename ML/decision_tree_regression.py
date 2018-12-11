66b9f86613e78f5480a8193531bd4d070c9dcf55
Kisaija Oscar <kisstark181@gmail.com>
2018-12-12 07:48:08 +0800

    Update decision_tree_regression.py

--- ML/decision_tree_regression.py
+++ ML/decision_tree_regression.py
@@ -1,11 +1,9 @@
 # Decision Tree Regression
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
@@ -15,7 +13,6 @@ from sklearn.tree import DecisionTreeRegressor
 regressor = DecisionTreeRegressor(random_state = 0)
 regressor.fit(X, y)
 
-# Predicting a new result
 y_pred = regressor.predict(6.5)
 
 # Visualising the Decision Tree Regression results (higher resolution)
