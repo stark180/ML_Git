108cd086ff2bf2592031da4ed401a37d14184875
Kisaija Oscar <kisstark181@gmail.com>
2018-12-12 06:03:00 +0800

    Update xgboost.py

--- ML/xgboost.py
+++ ML/xgboost.py
@@ -1,11 +1,9 @@
 # XGBoost
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
@@ -20,7 +18,6 @@ onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
@@ -29,10 +26,8 @@ from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
