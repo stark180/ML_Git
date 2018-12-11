7f02453096039068b6e232b6feaf4a6704394af2
Kisaija Oscar <kisstark181@gmail.com>
2018-12-12 05:59:13 +0800

    Update apriori.py

--- ML/Apriori_Python/apriori.py
+++ ML/Apriori_Python/apriori.py
@@ -1,11 +1,9 @@
 # Apriori
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Data Preprocessing
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
 transactions = []
 for i in range(0, 7501):
