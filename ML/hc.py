33b0c7397e40bf41d97321b992eeae5ee1a653e8
Kisaija Oscar <kisstark181@gmail.com>
2018-12-15 11:28:08 +0800

    Update hc.py

--- ML/hc.py
+++ ML/hc.py
@@ -1,11 +1,9 @@
 # Hierarchical Clustering
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
 # y = dataset.iloc[:, 3].values
@@ -23,7 +21,6 @@ from sklearn.cluster import AgglomerativeClustering
 hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
 y_hc = hc.fit_predict(X)
 
-# Visualising the clusters
 plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
 plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
 plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
