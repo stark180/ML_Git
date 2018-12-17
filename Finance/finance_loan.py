a76dc8b716a8c261f3671bfd0b7bacfb17ab8397
Kisaija Oscar <kisstark181@gmail.com>
2018-12-17 12:03:33 +0800

    SVM model updates

--- Finance/finance_loan.py
+++ Finance/finance_loan.py
@@ -122,3 +122,39 @@ f1 = f1_score(y_test, y_pred)
 # store the results in a pandas dataframe
 results = pd.DataFrame([['Linear Regression (lasso)', acc, prec, rec, f1]], 
                columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+## SVM (Linear)
+from sklearn.svm import SVC
+classifier = SVC(random_state=0, kernel = 'linear') 
+classifier.fit(X_train, y_train)
+# predicting the test set
+y_pred = classifier.predict(X_test)
+# determining the accurancy and performance
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+acc = accuracy_score(y_test, y_pred)
+prec =  precision_score(y_test, y_pred)
+rec =  recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+# store the results in a pandas dataframe
+model_results = pd.DataFrame([['SVM (Linear)', acc, prec, rec, f1]], 
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+# append the model results of the SVM to to the result dataframe
+results = results.append(model_results, ignore_index = True)
+
+## SVM (Gaussian)
+from sklearn.svm import SVC
+classifier = SVC(random_state=0, kernel = 'rbf')
+classifier.fit(X_train, y_train)
+# predicting the test set
+y_pred = classifier.predict(X_test)
+# determining the accurancy and performance
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+acc = accuracy_score(y_test, y_pred)
+prec =  precision_score(y_test, y_pred)
+rec =  recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+# store the results in a pandas dataframe
+model_results = pd.DataFrame([['SVM (Gaussian rbf)', acc, prec, rec, f1]], 
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+# append the model results of the SVM to to the result dataframe
+results = results.append(model_results, ignore_index = True)
