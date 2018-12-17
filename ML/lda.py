70bb701f8183d03a5ec5797e870e24a7985f886f
Kisaija Oscar <kisstark181@gmail.com>
2018-12-17 13:04:33 +0800

    Update lda.py

--- ML/lda.py
+++ ML/lda.py
@@ -1,16 +1,13 @@
 # LDA
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Wine.csv')
 X = dataset.iloc[:, 0:13].values
 y = dataset.iloc[:, 13].values
 
-# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
@@ -31,10 +28,8 @@ from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
