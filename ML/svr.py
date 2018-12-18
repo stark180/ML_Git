1ada574deb960590d88116d4805bce9aea0e89a1
Kisaija Oscar <kisstark181@gmail.com>
2018-12-18 08:52:23 +0800

    Update svr.py

--- ML/svr.py
+++ ML/svr.py
@@ -1,11 +1,9 @@
 # SVR
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
@@ -17,7 +15,6 @@ sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
-# Fitting SVR to the dataset
 from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
