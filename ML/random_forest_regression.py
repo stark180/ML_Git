e9ebe65765c3ac6681d614dce3e36ec992198b34
Kisaija Oscar <kisstark181@gmail.com>
2018-12-18 08:50:24 +0800

    Update random_forest_regression.py

--- ML/random_forest_regression.py
+++ ML/random_forest_regression.py
@@ -1,11 +1,9 @@
 # Random Forest Regression
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
@@ -15,7 +13,6 @@ from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
 regressor.fit(X, y)
 
-# Predicting a new result
 y_pred = regressor.predict(6.5)
 
 # Visualising the Random Forest Regression results (higher resolution)
