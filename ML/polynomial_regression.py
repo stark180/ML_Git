4298298e38dec38d7c9cdbb6e9dda769a3d3cd9d
Kisaija Oscar <kisstark181@gmail.com>
2018-12-18 08:49:05 +0800

    Update polynomial_regression.py

--- ML/polynomial_regression.py
+++ ML/polynomial_regression.py
@@ -1,16 +1,13 @@
 # Polynomial Regression
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-# Fitting Linear Regression to the dataset
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
