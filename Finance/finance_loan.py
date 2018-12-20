0602d55ca1db4f6cbf7074b3a9c820d3c07f1442
stark180 <kisstark181@gmail.com>
2018-12-20 12:36:09 +0800

    bullshit line added for fun

--- Finance/finance_loan.py
+++ Finance/finance_loan.py
@@ -17,6 +17,7 @@ dataset = pd.read_csv('financial_data.csv')
 
 # visualising the content of the dataset
 dataset.head()
+#bullshit line
 
 # visualising the statistics of the content in the dataset
 dataset.describe()
