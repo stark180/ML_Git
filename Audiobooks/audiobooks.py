48239b8ed8ad8c738b81ccc94fb8bb775f65cd67
stark180 <kisstark181@gmail.com>
2018-12-25 12:21:54 +0800

    preprocessing phase

--- Audiobooks/audiobooks.py
+++ Audiobooks/audiobooks.py
@@ -1,7 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 11:33:31 2018
+
+@author: oscar
+"""
+
+# importing the necessary libraries
 import numpy as np
 from sklearn import preprocessing
 
-raw_csv_data = np.loadtxt('Audiobooks_data.csv', delimiter = ',')
+raw_csv_data = np.loadtxt('Audiobooks_data.csv',delimiter=',')
 
 unscaled_inputs_all = raw_csv_data[:,1:-1]
+
 targets_all = raw_csv_data[:,-1]
+
+# balance the dataset before splitting
+# Count how many targets are 1 (meaning that the customer did convert)
+num_one_targets = int(np.sum(targets_all))
+
+# Set a counter for targets that are 0 (meaning that the customer did not convert)
+zero_targets_counter = 0
+
+indices_to_remove = []
+
+# Count the number of targets that are 0. 
+# Once there are as many 0s as 1s, mark entries where the target is 0.
+for i in range(targets_all.shape[0]):
+    if targets_all[i] == 0:
+        zero_targets_counter += 1
+        if zero_targets_counter > num_one_targets:
+            indices_to_remove.append(i)
+
+# Create two new variables, one that will contain the inputs, and one that will contain the targets.
+# We delete all indices that we marked "to remove" in the loop above.
+unscaled_inputs_equal_priors = np.delete(unscaled_inputs_all, indices_to_remove, axis=0)
+targets_equal_priors = np.delete(targets_all, indices_to_remove, axis=0)
+
+# standardize the inputs
+scaled_inputs = preprocessing.scale(unscaled_inputs_equal_priors)
+
+# shhuffle the data
+shuffled_indices = np.arange(scaled_inputs.shape[0])
+np.random.shuffle(shuffled_indices)
+
+# Use the shuffled indices to shuffle the inputs and targets.
+shuffled_inputs = scaled_inputs[shuffled_indices]
+shuffled_targets = targets_equal_priors[shuffled_indices]
+
+# split the data into training, validation and test sets
+# Count the total number of samples
+samples_count = shuffled_inputs.shape[0]
+
+# Count the samples in each subset, assuming we want 80-10-10 distribution of training, validation, and test.
+train_samples_count = int(0.8 * samples_count)
+validation_samples_count = int(0.1 * samples_count)
+
+# The 'test' dataset contains all remaining data.
+test_samples_count = samples_count - train_samples_count - validation_samples_count
+
+# Create variables that record the inputs and targets for training
+train_inputs = shuffled_inputs[:train_samples_count]
+train_targets = shuffled_targets[:train_samples_count]
+
+# Create variables that record the inputs and targets for validation.
+validation_inputs = shuffled_inputs[train_samples_count:train_samples_count+validation_samples_count]
+validation_targets = shuffled_targets[train_samples_count:train_samples_count+validation_samples_count]
+
+# Create variables that record the inputs and targets for test.
+test_inputs = shuffled_inputs[train_samples_count+validation_samples_count:]
+test_targets = shuffled_targets[train_samples_count+validation_samples_count:]
+
+# We balanced our dataset to be 50-50 (for targets 0 and 1), but the training, validation, and test were 
+# taken from a shuffled dataset. Check if they are balanced, too. 
+# Print the number of targets that are 1s, the total number of samples, and the proportion for training, validation, and test.
+print(np.sum(train_targets), train_samples_count, np.sum(train_targets) / train_samples_count)
+print(np.sum(validation_targets), validation_samples_count, np.sum(validation_targets) / validation_samples_count)
+print(np.sum(test_targets), test_samples_count, np.sum(test_targets) / test_samples_count)
+
+# save the three datasets in .npz formate
+np.savez('Audiobooks_data_train', inputs=train_inputs, targets=train_targets)
+np.savez('Audiobooks_data_validation', inputs=validation_inputs, targets=validation_targets)
+np.savez('Audiobooks_data_test', inputs=test_inputs, targets=test_targets)
